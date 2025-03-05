e827bbf83d410247a48dcd53e6e34f3f27a06146
Maryam Mirfazaelian <maryam.mirfazaelian@gmail.com>
2025-03-05 21:28:17 +0330

    dashboard created

--- src/run.py
+++ src/run.py
@@ -1,1 +1,18 @@
 import streamlit as st
+
+
+st.title("Pytopia Dashboard")
+
+with st.sidebar:
+    login_status = st.radio(options=["Login", "Sign Up"], label="Login/Signup")
+    if login_status == "Login":
+        st.text_input(label="Username", placeholder='Enter your username')
+        st.text_input(label="Password", placeholder='Enter your password')
+    else:
+        st.text_input(label="Choose a username", placeholder="Username")
+        st.text_input(label="Choose a email", placeholder="Email")
+        st.text_input(label="Write a password", placeholder="Password")
+
+
+with st.expander(label="Doc"):
+    st.file_uploader(label="Choose a file")
